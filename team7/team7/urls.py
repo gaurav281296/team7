2bc938d7bcc8b8bbdcb1359eb92f74acf6f5e892
Gaurav Agarwal <gaurav.agarwal28.12.96@gmail.com>
2019-09-29 19:22:12 +0530

    Changed url paths.

--- team7/team7/urls.py
+++ team7/team7/urls.py
@@ -22,8 +22,9 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Tracker API')
 
 urlpatterns = [
-    path('', schema_view),
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
+    path('docs', schema_view),
     path('task/', views.TaskList.as_view()),
     path('task/<int:pk>/', views.TaskDetail.as_view()),
     path('project/', views.ProjectList.as_view()),
